c9b143fa526594b595e605460c061cfc49422ad4
kolla47 <akhil.trinayan@gmail.com>
2018-09-10 00:46:10 +0530

    1st commit'

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var token = '5322494348.f5fe872.4fe04e2c71704e36b853e72ba4d34859',
+    hashtag='hydtrailblazin',
+    num_photos = 9;
+
+$.ajax({
+	url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: token, count: num_photos},
+	success: function(data){
+		console.log(data);
+		for(x in data.data){
+			$('ul').append('<li><img style="width: 150px; height: 125px;" src="'+data.data[x].images.standard_resolution.url+'"></li>');
+		}
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
